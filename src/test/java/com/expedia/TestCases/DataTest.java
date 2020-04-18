37d1127c6ebe5dee6f11b13c7ffde7956f7d5c60
Zak Chekir <zchekir@cogstate.com>
2020-04-18 12:38:38 -0400

    Added excel dependencies

---
+++ src/test/java/com/expedia/TestCases/DataTest.java
@@ -0,0 +1,13 @@
+package com.expedia.TestCases;
+
+import org.testng.annotations.Test;
+
+public class DataTest {
+
+	@Test
+	public void ExcelData() {
+		
+	//	XSSFworkbook  ExcelObjec = new 	XSSFworkbook();
+		
+	}
+}
