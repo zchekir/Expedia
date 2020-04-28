f1c3af12d8dbb227997cc587780f8b9ca2c19643
zeinkap <zeinkap@gmail.com>
2020-04-28 16:37:00 -0400

    # added try-catch block to DataTest.java file

--- src/test/java/com/expedia/TestCases/DataTest.java
+++ src/test/java/com/expedia/TestCases/DataTest.java
@@ -19,33 +19,42 @@ public class DataTest extends Helper {
 	public static void ReadExcelData() throws Exception {
 		
 		OpenBrowser();
-		FileInputStream ExcelFile = new FileInputStream(System.getProperty("user.dir") + "/src/main/resources/TestData.xlsx");
-	
-		workbook = new XSSFWorkbook(ExcelFile);
-		sheet = workbook.getSheetAt(0);
+		try {
+			FileInputStream ExcelFile = new FileInputStream(System.getProperty("user.dir") + "/src/main/resources/TestData.xlsx");
 		
-		for (int i = 1; i <= sheet.getLastRowNum(); i++) {
-			// Import second row, first cell data
-	        cell = sheet.getRow(i).getCell(0);
-	        cell.setCellType(CellType.STRING);
-	        WebElement destination = driver.findElement(By.xpath("//input[@id='hotel-destination-hp-hotel']"));
-	        destination.sendKeys(cell.getStringCellValue());
+			workbook = new XSSFWorkbook(ExcelFile);
+			sheet = workbook.getSheetAt(0);
+			
+			for (int i = 1; i <= sheet.getLastRowNum(); i++) {
+				// Import second row, first cell data
+		        cell = sheet.getRow(i).getCell(0);
+		        cell.setCellType(CellType.STRING);
+		        WebElement destination = driver.findElement(By.xpath("//input[@id='hotel-destination-hp-hotel']"));
+		        destination.sendKeys(cell.getStringCellValue());
+		
+		        // Import second row, second cell data
+		        cell = sheet.getRow(i).getCell(1);
+		        cell.setCellType(CellType.NUMERIC);	// date stored as numeric
+		        String cellStringValue = dataFormatter.formatCellValue(cell);
+		        //System.out.println("data in excel file: " + cellStringValue);
+		        WebElement checkIn = driver.findElement(By.xpath("//input[@id='hotel-checkin-hp-hotel']"));
+		        checkIn.sendKeys(cellStringValue);
 	
-	        // Import second row, second cell data
-	        cell = sheet.getRow(i).getCell(1);
-	        cell.setCellType(CellType.NUMERIC);	// date stored as numeric
-	        String cellStringValue = dataFormatter.formatCellValue(cell);
-	        //System.out.println("data in excel file: " + cellStringValue);
-	        WebElement checkIn = driver.findElement(By.xpath("//input[@id='hotel-checkin-hp-hotel']"));
-	        checkIn.sendKeys(cellStringValue);
-	        
-	        // Import second row, third cell data
-	        
+		        workbook.close();
+		        driver.close();
+			}
 		}
+		
+		catch(Exception e) {
+			System.out.println(e);
+		}
+			
+			
+		
 		
 	}
 	
-	@Test
+	//@Test
 	public void WriteExcelData() {
 		
 	}
